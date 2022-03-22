a121285fe96e877a7c79d603995e79cfc677cabd
carlos silva <carlossilva@MacBook-Pro-de-carlos.local>
2022-03-22 18:00:16 -0300

    segundo

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { GlobalStyle } from "./styles/global";
 export function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <h1>Hello world com estilos pra variar</h1>
       <GlobalStyle />
     </div>
   );
