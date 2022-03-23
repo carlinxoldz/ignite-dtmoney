f5d046a8bb7519be5df0504cb34516aa7177b3fa
carlos silva <carlossilva@MacBook-Pro-de-carlos.local>
2022-03-23 20:38:22 -0300

    terceiro

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,10 +5,11 @@ export const GlobalStyle = createGlobalStyle`
     --background: #f0f2f5;
     --red: #e52e4d;
     --blue: #5429cc;
+    --green: #33cc95;
 
     --blue-light: #6933ff;
 
-    --text-title: #356f5f;
+    --text-title: #363f5f;
     --text-body: #969bc3;
 
     --shape: #ffffff;
@@ -33,6 +34,15 @@ export const GlobalStyle = createGlobalStyle`
   body{
     background: var(--background);
     -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
   }
 
   button {
